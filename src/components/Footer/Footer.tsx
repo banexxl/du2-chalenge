a5e8b0e76706eaffd28c9691d9d46e563191f214
banexxl <damjanovic.branislav@gmail.com>
2022-12-01 14:17:44 +0100

    sto se mene tice footer gotov :D

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import styles from './footer.module.css';
 import logo from '../../assets/images/L9200x200.png';
+import { TbBrandFacebook } from "react-icons/tb"
+import { AiOutlineLink } from "react-icons/ai"
+import { SiLinkedin } from "react-icons/si"
+import { BsTwitter } from "react-icons/bs"
+import { SiYoutube } from "react-icons/si"
 
 const Footer = () => {
           return (
                     <footer className={styles.footer}>
                               <img src={logo} alt="logo" width="200" className={styles.logo} />
+                              <div className={styles.footer_links}>
+                                        <div>
+                                                  <a href='https://www.levi9.com/'>
+                                                            <AiOutlineLink />
+                                                            Levi9 Homepage
+                                                  </a>
+                                        </div>
+                                        <div>
+                                                  <a href='https://www.facebook.com/Levi9Serbia/'>
+                                                            <TbBrandFacebook />
+                                                            Facebook
+                                                  </a>
+                                        </div>
+                                        <div>
+                                                  <a href='https://www.linkedin.com/company/levi9/mycompany/'>
+                                                            <SiLinkedin />
+                                                            LinkedIn
+                                                  </a>
+                                        </div>
+                                        <div>
+                                                  <a href='https://twitter.com/Levi9company'>
+                                                            <BsTwitter />
+                                                            Twitter
+                                                  </a>
+                                        </div>
+                                        <div>
+                                                  <a href='https://www.youtube.com/@Levi9ITServicesNearshore'>
+                                                            <SiYoutube />
+                                                            YouTube
+                                                  </a>
+                                        </div>
+                              </div>
                     </footer>
           )
 }
