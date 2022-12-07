a51d7e5256fc5acbda536b97474490db1b084c87
banexxl <damjanovic.branislav@gmail.com>
2022-12-07 14:32:58 +0100

    test odradjen :)

---
+++ test/cypress/e2e/spec.cy.js
@@ -0,0 +1,39 @@
+describe('Jedan obican test :)', () => {
+          it('passes', () => {
+                    cy.visit('http://localhost:3000/')
+
+                    cy.get(':nth-child(1) > .button_btn__7rFQD').click()
+                    cy.get(':nth-child(3) > .button_btn__7rFQD').click()
+                    cy.get(':nth-child(8) > .button_btn__7rFQD').click()
+
+                    cy.get(':nth-child(8) > .button_btn__7rFQD').click()
+
+                    cy.get(':nth-child(8) > .button_btn__7rFQD').click()
+                    cy.get(':nth-child(8) > .button_btn__7rFQD').click()
+
+                    cy.get('.container_container__wmKTy > :nth-child(1) > .headerStyles_header_content__tAwG5 > :nth-child(3) > a').click()
+
+                    cy.get(':nth-child(2) > .quantitybutton_quantity__i1iBn > :nth-child(3)').click()
+                    cy.get(':nth-child(2) > .quantitybutton_quantity__i1iBn > :nth-child(3)').click()
+
+
+                    cy.get('a > .button_btn__7rFQD').click()
+
+                    cy.get('#firstName').type("Branislav")
+                    cy.get('#lastName').type("Damjanovic")
+                    cy.get('#streeAddress').type("Bulevar Oslobodjenja")
+                    cy.get('#city').type("Novi Sad")
+                    cy.get('#zipCode').type("21000")
+                    cy.get('#phone').type("066415651")
+                    cy.get('#email').type("sdsd")
+
+                    cy.get('#phone').click()
+
+                    cy.get('.form_error__cReGm').contains("Please enter a valid email")
+
+                    cy.get('#email').clear()
+                    cy.get('#email').type("damjanovic.branislav@gmail.com")
+                    cy.get('button').click()
+
+          })
+})
